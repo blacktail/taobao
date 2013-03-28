e9f31f9089d8316e6506b6ca5505206076f6ab1a
blacktail <mr.zhangping@gmail.com>
2013-03-28 15:01:46 +0800

    add user api

--- taobao.js
+++ taobao.js
@@ -1,9 +1,20 @@
-var core = require('./lib/core');
+var _ = require('underscore'),
+	core = require('./lib/core'),
+	user = require('./lib/user');
+
 
 module.exports = {
 	config: function (config) {
 		core.config(config);
 	},
 
+	updateSession: function(session) {
+		core.config({
+			session: session
+		});
+	},
+
 	core: core
-}
+};
+
+_.extend(module.exports, user);
